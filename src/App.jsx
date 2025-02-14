82378aa69b9ddcbe16d750cdcb75b14480881821
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-15 02:44:29 +0600

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Blogs from './components/Blogs/Blogs';
 import Header from './components/Header/Header';
 import './index.css'
 function App() {
   return (
     <div>
       <Header></Header>
+      <Blogs></Blogs>
     </div>
   )
 }
