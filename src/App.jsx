2974259668ceb51a16e5dd1596a73c05d35fb1ff
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-15 02:31:22 +0600

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-export default function App() {
+import Header from './components/Header/Header';
+import './index.css'
+function App() {
   return (
     <div>
-      <h1 className='text-white-500 bg-skyblue-500 text-3xl mt-2 mb-12'>
-        Hello world!
-      </h1>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <Header></Header>
     </div>
-  );
+  )
 }
+
+export default App;
