e8c5292ac1bd28db580a59e58bb09735d342bc58
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-15 16:04:25 +0600

    added new file code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Blogs from './components/Blogs/Blogs';
+import Bookmarks from './components/Bookmarks/Bookmarks';
 import Header from './components/Header/Header';
-import './index.css'
+import './index.css';
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Blogs></Blogs>
-    </div>
+    <>
+      <div>
+        <Header></Header>
+      </div>
+      <div className='md:flex '>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </>
   )
 }
 
