a43d2b0089d70f294729cf3d693a7919168b0e81
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-15 01:04:41 +0600

    added updated file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,7 @@ const Blog = ({ blog }) => {
     // console.log(blog);
     const { title, cover, author, author_image, posted_date, reading_time, hashtags } = blog;
     return (
-        <div className='mb-2'>
+        <div className='mb-2 p-4'>
             <img className='p-4 w-full mb-8' src={cover} alt={`cover picture of the title ${title}`} />
             <div className='flex justify-between mb-4'>
                 <div className='p-4'>
@@ -14,9 +14,6 @@ const Blog = ({ blog }) => {
                         <div>
                             <span>{posted_date}</span>
                         </div>
-                        <div>
-                            <span>{hashtags}</span>
-                        </div>
                     </div>
                 </div>
                 <div>
@@ -24,7 +21,14 @@ const Blog = ({ blog }) => {
                 </div>
             </div>
             <h2 className="text-3xl">{title}</h2>
-
+            <p>
+                {
+                    hashtags.map((hash, index) =>
+                        <span key={index}>
+                            <a href="">#{hash}</a>
+                        </span>)
+                }
+            </p>
         </div>
     );
 };
