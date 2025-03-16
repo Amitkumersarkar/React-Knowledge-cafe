f4c3cdbed1f10259bf8c63135803cd97adde56ac
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-16 14:05:15 +0600

    added new file

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -4,8 +4,8 @@ const Bookmark = ({ bookmark }) => {
     const { title } = bookmark;
 
     return (
-        <div>
-            <h3 className="text-3xl">{title}</h3>
+        <div className="bg-cyan-700 p-4 m-4 rounded-2xl">
+            <h3 className="text-2xl">{title}</h3>
         </div>
     );
 };
